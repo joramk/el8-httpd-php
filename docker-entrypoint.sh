d9c9f5a528a9a464a86496b7871669be84fb5c5f
joramk <joramk@users.noreply.github.com>
2017-11-13 10:50:42 +0100

    Removed SSL certificate creation

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -27,19 +27,8 @@ setup() {
         fi
 }
 
-answers() {
-        echo --
-        echo SomeState
-        echo SomeCity
-        echo SomeOrganization
-        echo SomeOrganizationalUnit
-        echo localhost.localdomain
-        echo root@localhost.localdomain
-}
-
 if [ -e /firstrun ] && [ -z "$HTTPD_OMIT_FIRSTRUN" ]; then
         setup
-        answers | /usr/bin/openssl req -newkey rsa:2048 -keyout /etc/pki/tls/private/localhost.key -nodes -x509 -days 730 -out /etc/pki/tls/certs/localhost.crt
         rm -f /firstrun
 fi
 
